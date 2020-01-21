fa96f2961fbaf113b9e170195bc2305b43678e26
AmmonStretz <ammon-mino@web.de>
2020-01-21 20:19:21 +0100

    change structure and diagrams

---
+++ noHate/install.sh
@@ -0,0 +1,8 @@
+pip install jupyterlab
+pip install spacy
+python -m spacy download de_core_news_sm
+pip install template
+pip install matplotlib
+pip install pandas
+#pip install nbimporter
+pip install spellchecker
